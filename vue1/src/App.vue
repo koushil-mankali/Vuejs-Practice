8a6067e22c37049a74ffbb742b9133669bb512ff
koushil-mankali <koushilmankali@gmail.com>
2022-09-28 16:52:02 +0530

    Computed property done

--- vue1/src/App.vue
+++ vue1/src/App.vue
@@ -1,187 +1,35 @@
 <script lang="ts">
-function inc(this: { count: number }, event: Event, incVal: number) {
-  console.log(event, "Event");
-  return (this.count += incVal || 1);
+function state() {
+  return {
+    firstName: "First Name",
+    lastName: "Lsat Name",
+  };
 }
 
-function dec(this: { count: number }) {
-  if (this.count === 0) {
-    return alert("Count reached minimum value!");
-  }
-  this.count -= 1;
+function fullname(this: { firstName: string; lastName: string }) {
+  console.log("Called method");
+  return this.firstName + " " + this.lastName;
 }
-
-function submitForm(
-  this: {
-    formValues: {
-      name: string;
-      about: string;
-      graduate: boolean;
-      education: [];
-      exp: string;
-    };
-  },
-  e: Event
-) {
-  e.preventDefault();
-  console.log(this.formValues, "values");
+function fullnamec(this: { firstName: string; lastName: string }) {
+  console.log("Called computed");
+  return this.firstName + " " + this.lastName;
 }
 
 export default {
-  data: () => {
-    return {
-      count: 0,
-      formValues: {
-        name: "",
-        about: "",
-        graduate: false,
-        education: [],
-        exp: '',
-      },
-    };
-  },
+  data: state,
   methods: {
-    inc,
-    dec,
-    submitForm,
+    fullname,
+  },
+  computed: {
+    fullnamec,
   },
 };
 </script>
 
-<!-- <template>
-  <div>
-    <div class="m1">Welcome to Vue!</div>
-    <div class="m1 crimson">Count {{ count }}</div>
-    <button class="button" @:click="inc($event)">Inc</button>
-    <button class="button" @:click="dec()">dec</button>
-    <button class="button" @:dblclick="inc($event, 5)">Inc</button>
-  </div>
- 
-</template> -->
-
 <template>
-  <div>
-    <div>
-      <div v-once>This is Once only rendered</div>
-      <div v-pre>{{count}}</div>
-      <div class="m1">Welcome to Vue!</div>
-      <div class="m1 crimson">Count {{ count }}</div>
-      <button class="button" @:click="inc($event)">Inc</button>
-      <button class="button" @:click="dec()">dec</button>
-      <button class="button" @:dblclick="inc($event, 5)">Inc</button>
-    </div>
-    <form @submit="submitForm" class="form">
-      <div>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" v-model.lazy.trim="formValues.name" />
-      </div>
-      <div>
-        <label for="about">About</label>
-        <textarea name="about" id="about" v-model.lazy.trim="formValues.about" />
-      </div>
-      <div>
-        <label for="graduate">Graducate</label>
-        <input
-          type="checkbox"
-          name="graduate"
-          id="graduate"
-          v-model="formValues.graduate"
-        />
-      </div>
-      <div>
-        <label for="education">Education</label>
-        <div>
-          <input
-            type="checkbox"
-            name="ssc"
-            id="ssc"
-            value="SSC"
-            v-model="formValues.education"
-          />
-          <label for="ssc">SSC</label>
-          <input
-            type="checkbox"
-            name="inter"
-            id="inter"
-            value="Inter"
-            v-model="formValues.education"
-          />
-          <label for="inter">Inter</label>
-          <input
-            type="checkbox"
-            name="btech"
-            id="btech"
-            value="BTECH"
-            v-model="formValues.education"
-          />
-          <label for="btech">BTECH</label>
-        </div>
-      </div>
-      <div>
-        <label for="exp">Experience:</label>
-        <div>
-          <input
-            type="radio"
-            name="1"
-            id="1"
-            v-model="formValues.exp"
-            value="1"
-          />
-          <label for="1">1 Year</label>
-
-          <input
-            type="radio"
-            name="2"
-            id="2"
-            v-model="formValues.exp"
-            value="2"
-          />
-          <label for="2">2 Year</label>
-
-          <input
-            type="radio"
-            name="3"
-            id="3"
-            v-model="formValues.exp"
-            value="3"
-          />
-          <label for="3">3 Year</label>
-        </div>
-      </div>
-      <button>Submit</button>
-    </form>
-  </div>
+  <div>Mushtash: {{ firstName }} {{ lastName }}</div>
+  <div>Method: {{ fullname() }}</div>
+  <div>computed: {{ fullnamec }}</div>
 </template>
 
-<style scoped>
-.m1 {
-  margin: "1rem";
-}
-
-.crimson {
-  color: crimson;
-}
-
-.button {
-  padding: 10px;
-  border-radius: 5px;
-  background-color: yellow;
-  margin: 5px;
-  font-size: 1rem;
-  font-weight: 600;
-  cursor: pointer;
-}
-
-.form {
-  width: 300px;
-  height: 300px;
-  margin: auto;
-  padding: 10px;
-  border: 1px solid white;
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-}
-</style>
+<style scope></style>
