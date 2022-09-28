daa4ff97fb9b1f37a381fc41290422531519254d
koushil-mankali <koushilmankali@gmail.com>
2022-09-28 11:17:46 +0530

    Mustash{{}}, v-text, v-html, v-bind done

--- vue1/src/App.vue
+++ vue1/src/App.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+let state = () => {
+  let btnTxt = "Click Me";
+  return {
+    greet: "Hell",
+    subject: "World!",
+    buttonId: "button",
+    classes: "test test1",
+    isDisabled: true,
+    test: "This app is beign developed with the help of Vue!",
+    html: `<button onclick="alert('hi')">${btnTxt}</button>`,
+  };
+};
+export default {
+  data: state,
+};
+</script>
 
 <template>
-  <div>Hello Vue!</div>
+  <div>
+    <div>{{ greet }} {{ subject }}</div>
+    <div v-text="test"></div>
+    <button v-bind:disabled="isDisabled">Disabled Button</button>
+    <div v-html="html" v-bind:id="buttonId" v-bind:class="classes"></div>
+  </div>
 </template>
 
 <style scoped></style>
