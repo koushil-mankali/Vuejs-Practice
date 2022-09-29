933c207746e76afb2a4a5d96173e99f09bb7fbcf
koushil-mankali <koushilmankali@gmail.com>
2022-09-29 16:17:45 +0530

    v2 Started Components

---
+++ vue2/src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Home from './components/Home.vue'
+
+  export default {
+    components: {
+      Home,
+    }
+  }
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style>
+  body{
+    height: 100vh;
+    background-color: #42B883;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ vue2/src/components/Home.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  name: "Home",
+  data() {
+    return {
+      txt: "Home Component",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ txt }}</h1>
+</template>
+
+<style scoped></style>
