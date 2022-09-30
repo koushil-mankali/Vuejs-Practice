303f43bfc6ee0e16e958299b26c9b734e0726000
koushil-mankali <koushilmankali@gmail.com>
2022-09-30 14:49:05 +0530

    API calls done

---
+++ vue2/src/components/Posts.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+    export default{
+        name: "Posts",
+        props: {
+            post: Object,
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <h1>{{post.id}}. {{post.title}}</h1>
+        <p>{{post.body}}</p>
+    </div>
+</template>
+
+<style scoped>
+    .card{
+        width: 90%;
+        height: 200px;
+        padding: 10px;
+        margin: 0.5rem;
+        border-radius: 5px;
+        border: 1px solid #eee;
+        box-shadow: 2px 2px 2px #eee;
+    }
+</style>
